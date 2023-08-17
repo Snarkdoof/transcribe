34555452dda632084e3c44a8fd7fb3ef2cb762a5
Njål Borch <njaal.borch@norut.no>
2023-08-17 09:16:24 +0200

    Minor polish, stop after 5 minutes, docker file should now work

--- workernode/onstart.sh
+++ workernode/onstart.sh
@@ -15,12 +15,15 @@ sudo chown -R cryocore /tmp
 rsync -r /data/models /scratch/ --size-only
 sudo chown cryocore -R /scratch/
 
-cd /home/cryocore/git/transcribe
+CODE_DIR=/home/cryocore/git/transcribe
+cd $CODE_DIR
+
+echo "Updating code base"
 sudo -u cryocore git pull
 
-screen -L -Logfile /home/cryocore/git/transcribe/workernode/stopifidle.term -d -m -S stopifidle /home/cryocore/git/transcribe/workernode/stopifidle
+screen -L -Logfile $CODE_DIR/workernode/stopifidle.term -d -m -S stopifidle $CODE_DIR/workernode/stopifidle
 
 echo "Starting nodes"
 
-sudo -u cryocore /home/cryocore/git/transcribe/workernode/start_workers.sh
+sudo -u cryocore $CODE_DIR/workernode/start_workers.sh
 echo "Startup OK"
