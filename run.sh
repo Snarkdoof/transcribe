3d59446dcb6a8fd7a710359fa409b40a70160cb2
Njål Borch <njaal.borch@norut.no>
2023-09-04 11:09:46 +0200

    Handle any kind of exception - should never stop!

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ccworkflow transcribe2.json --dir /home/njaal/git/cryonite/microservices/transcribe/ --src $1 --dst /data/cache/$2 ${@:3}
--- runtranscribe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=$PYTHONPATH:~/.local/
-python3 -m w2vtranscriber.transcription_pipeline 
-
